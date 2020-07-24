11215fd8ab56b483dee85bdd526da4dc12324bd8
Mark Retallack <mark.retallack@siemens.com>
2020-07-24 09:38:13 +0100

    Added photo push to telegram

--- src/notifications/telegram.js
+++ src/notifications/telegram.js
@@ -1,5 +1,8 @@
 /*eslint @typescript-eslint/camelcase: ["error", {properties: "never"}]*/
 const telegram = require("telegram-bot-api");
+const fs = require('fs');
+const mergeImages = require('merge-images');
+const { Canvas, Image } = require('canvas');
 
 /** @typedef {import("../index").Store} Store */
 
@@ -17,25 +20,15 @@ class TelegramNotifier {
     }
   }
 
-  _sendMessage(api, chatId, msg, screenshot) {
+  _sendMessage(api, chatId, msg) {
     api.sendMessage({
       chat_id: chatId,
       text: msg,
       parse_mode: "HTML",
     });
-
-const fileOptions = {
-  // Explicitly specify the file name.
-  filename: 'screen.jpeg',
-  // Explicitly specify the MIME type.
-  contentType: 'image/jpeg',
-};
-
-//console.log(screenshot)
-    //api.sendPhoto({chatId: chatId, photo: screenshot, options: {}, fileOptions: fileOptions});
   }
 
-  async sendMessage(msg, screenshot) {
+  async sendMessage(msg) {
     const api = new telegram({ token: this.config.telegram_api_token, polling: true});
     const updates = await api.getUpdates({});
     if (updates && updates.length != 0) {
@@ -47,7 +40,27 @@ const fileOptions = {
       });
     }
     Object.keys(this.chatIdsCache).forEach((chatId) => {
-      this._sendMessage(api, chatId, msg, screenshot);
+      this._sendMessage(api, chatId, msg);
+    });
+  }
+
+  async sendScreenshot(screenshot) {
+    const api = new telegram({ token: this.config.telegram_api_token, polling: true});
+    const updates = await api.getUpdates({});
+    if (updates && updates.length != 0) {
+      const chatIds = updates.map((update) =>
+        this._extractChatIdFromUpdate(update)
+      );
+      chatIds.forEach((chatId) => {
+        this.chatIdsCache[chatId] = 1;
+      });
+    }
+    Object.keys(this.chatIdsCache).forEach((chatId) => {
+          fs.writeFileSync("screenshot.png", screenshot, 'base64');
+          api.sendPhoto({
+              chat_id: chatId,
+              photo: "screenshot.png"
+            });
     });
   }
 
@@ -69,10 +82,38 @@ const fileOptions = {
    */
   async sendNotifications(store, type, slotDates) {
     if (slotDates && slotDates.length != 0) {
+      var images=[];
+      var x=0;
+      var y=0;
+      var imgId=0;
       for (const slotDate of slotDates) {
         const msg = this.generateMessage(store, type, slotDate);
-        this.sendMessage(msg, slotDate.screenshot);
+        this.sendMessage(msg);
+
+        var imgName='screenshot'+imgId+'.png'
+        fs.writeFileSync(imgName, slotDate.screenshot)
+        images.push({src: imgName, x: x*1000, y: y*1000});
+        x=x+1;
+        imgId=imgId+1;
+        if (x > 1)
+        {
+            x=0;
+            y=y+1;
+        }
       }
+
+      var merged=mergeImages(images, {
+              Canvas: Canvas,
+              Image: Image,
+              width: 2000,
+              height: 2000
+            }).then(b64 => {
+                var base64Data = b64.replace(/^data:image\/png;base64,/, "");
+
+                this.sendScreenshot(base64Data);
+            });
+
+      fs.writeFileSync("merged", merged)
     } else {
       this.sendMessage(`no slots`);
     }
